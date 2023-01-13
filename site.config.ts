de57118aba0a917a9bf30fa8b92d8bd1e0a64079
Alphaspiderman <68412330+Alphaspiderman@users.noreply.github.com>
2023-01-13 16:01:10 +0530

    Add Config for Main Site

--- site.config.ts
+++ site.config.ts
@@ -2,23 +2,23 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
 	// the site's root Notion page (required)
-	rootNotionPageId: '',
+	rootNotionPageId: '8303c68ae96f428f8e66c486706a78e9',
 
 	// if you want to restrict pages to a single notion workspace (optional)
 	// (this should be a Notion ID; see the docs for how to extract this)
 	rootNotionSpaceId: null,
 
 	// basic site info (required)
-	name: '',
-	domain: '',
-	author: '',
+	name: 'Home - Alphaspiderman',
+	domain: 'alphaspiderman.dev',
+	author: 'Alphaspiderman',
 
 	// open graph metadata (optional)
 	description: '',
 
 	// social usernames (optional)
-	twitter: '',
-	github: '',
+	twitter: '_Alphaspiderman',
+	github: 'Alphaspiderman',
 	linkedin: '',
 	// mastodon: '#', // optional mastodon profile URL, provides link verification
 	// newsletter: '#', // optional newsletter URL
@@ -46,20 +46,22 @@ export default siteConfig({
 	//   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
 	//   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
 	// }
-	pageUrlOverrides: null,
+	pageUrlOverrides: {
+		'/about': '6923667bf68c48cfaf7ee5affbc2f162',
+		'/contact': 'ff7a1c669ee04178a3b913e5e113d114'
+	},
 
 	// whether to use the default notion navigation style or a custom one with links to
 	// important pages
-	navigationStyle: 'default'
-	// navigationStyle: 'custom',
-	// navigationLinks: [
-	//   {
-	//     title: 'About',
-	//     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-	//   },
-	//   {
-	//     title: 'Contact',
-	//     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-	//   }
-	// ]
+	navigationStyle: 'custom',
+	navigationLinks: [
+		{
+			title: 'About',
+			pageId: '6923667bf68c48cfaf7ee5affbc2f162'
+		},
+		{
+			title: 'Contact',
+			pageId: 'ff7a1c669ee04178a3b913e5e113d114'
+		}
+	]
 })
